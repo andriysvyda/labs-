73a3a505eb402710c5f5932d479d0093a5d86567
andriy <93590785+andriysvyda@users.noreply.github.com>
2023-05-26 14:00:09 +0300

    Add files via upload

---
+++ lab5/DemoWebApp/Controllers/ChatsController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using DemoWebApp.Services.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace DemoWebApp.Controllers
+{
+    [ApiController]
+    [Authorize]
+    public class ChatsController : ControllerBase
+    {
+        public ChatsController(IRepository<Chat> service)
+        {
+            this.service = (ChatsRepositoryService)service;
+        }
+
+        [HttpGet("api/chats")]
+        public IActionResult Read(
+            [FromQuery] string orderBy = "Id",
+            [FromQuery] string order = "asc",
+            [FromQuery] int page = 1,
+            [FromQuery] int perPage = 25)
+        {
+            // to-do: додати можливість отримувати cписок доступних для користувача чатів
+            var userId = Helpers.AuthHelper.GetUserId(HttpContext.User);
+
+            var chats = service.GetChatsForUser(userId, orderBy, order, page, perPage);
+
+            return Ok();
+        }
+
+        [HttpPost("api/chats")]
+        public IActionResult Create()
+        {
+            // to-do: додати можливість створювати чат
+            var chat = new Chat();
+            service.Create(chat);
+
+            return Ok();
+        }
+
+        [HttpDelete("api/chats/{id}")]
+        public IActionResult Delete(int id)
+        {
+            // to-do: додати можливість видаляти чат
+             var userId = Helpers.AuthHelper.GetUserId(HttpContext.User);
+
+            if (service.CanUserDeleteChat(id, userId))
+            {
+                service.Delete(id);
+
+                return Ok();
+            }
+        }
+
+        [HttpGet("api/chats/{chatId}/messages")]
+        public IActionResult ReadMessages(
+            int chatId,
+            [FromQuery] string orderBy = "Id",
+            [FromQuery] string order = "asc",
+            [FromQuery] int page = 1,
+            [FromQuery] int perPage = 25)
+        {
+            // to-do: додати можливість отримувати список повідомлень у чаті
+            var messages = service.GetMessagesForChat(chatId, orderBy, order, page, perPage);
+
+            return Ok();
+        }
+    }
+}
---
+++ lab5/DemoWebApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DemoWebApp.Models;
+
+namespace DemoWebApp.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Post> Posts => Set<Post>();
+        public DbSet<CommentToPost> Comments => Set<CommentToPost>();
+
+        /* to-do: додати колекції для чатів та меседжів */
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            //Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Post>()
+                .HasMany(post => post.Comments)
+                .WithOne(comment => comment.Post)
+                .HasForeignKey(comment => comment.PostId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<User>()
+                .HasMany(user => user.Posts)
+                .WithOne(post => post.User)
+                .HasForeignKey(post => post.UserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<User>()
+                .HasMany(user => user.CommentToPosts)
+                .WithOne(comment => comment.User)
+                .HasForeignKey(comment => comment.UserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.NoAction);
+
+            /* to-do: додати опис моделей для чатів та меседжів */
+        }
+    }
+}
